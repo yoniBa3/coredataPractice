f3b66e2265210008619eaca5732d6b52e40800d5
yoniBa3 <yoniba3@gmail.com>
2020-06-03 22:01:41 +0300

    First upload

---
+++ coredataPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coredataPractice
+//
+//  Created by Yoni on 03/06/2020.
+//  Copyright © 2020 Yoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
